2ed7f3121358a5e002e0660d1adea8d4caf9ba3c
feikkont <feikkont@gmail.com>
2022-08-11 14:53:03 +1100

    Написать веб-версию нашего cli приложения для просмотра каталогов и чтения файлов

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,89 @@
 import './App.css';
-import React, {useState, useEffect, useCallback} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {MyTable} from "./table";
 
 function App() {
     const [fileList, setFileList] = useState([])
-    console.log('filelist', fileList)
-    useEffect(() => {
-        fetch('http://localhost:4000/')
-            .then(res => res.json())
+    const [fileData, setFileData] = useState('')
+
+    const getData = (url = '/', open = false) => {
+        // debugger
+        return fetch(`http://localhost:4000${url}`)
+            .then(res => !open ? res.json() : res.text())
             .then((result) => {
-                console.log('result', result)
-                setFileList(result);
-            })
-    },[])
+                if (!open) {
+                    result.sort((a, b) => {
+                        if (a.size < b.size) {
+                            return -1;
+                        }
+                        if (a.size < b.size) {
+                            return 1;
+                        }
+                        return 0
+                    })
+                    setFileList(result);
+                } else {
+                    setFileData(result);
+                }
+            }
+        )
+    }
+
+    useEffect(() => {
+        getData()
+        // fetch('http://localhost:4000/')
+        //     .then(res => res.json())
+        //     .then((result) => {
+        //         console.log('result', result)
+        //         result.sort((a, b) => {
+        //             if (a.size < b.size) {
+        //                 return -1;
+        //             }
+        //             if (a.size < b.size) {
+        //                 return 1;
+        //             }
+        //             return 0
+        //         })
+        //         setFileList(result);
+        //     })
+    }, [])
 
     const clickHandler = useCallback((item) => {
-        // debugger
-        if(item.fileName == 'back' || item.isDir) {
-            fetch(`http://localhost:4000/?path=${item.filePath}`)
-                .then(res => res.json())
-                .then((result) => {
-                    console.log('result', result)
-                    setFileList(result);
-                })
+        if (item.fileName == 'back' || item.isDir) {
+            getData(`/?path=${item.filePath}`)
+            // fetch(`http://localhost:4000/?path=${item.filePath}`)
+            //     .then(res => res.json())
+            //     .then((result) => {
+            //         result.sort((a, b) => {
+            //             if (a.size < b.size) {
+            //                 return -1;
+            //             }
+            //             if (a.size < b.size) {
+            //                 return 1;
+            //             }
+            //             return 0
+            //         })
+            //         setFileList(result);
+            //     })
         } else {
-            fetch(`http://localhost:4000/?open=${item.filePath}`)
-                .then(res => {
-                    // debugger
-                    return res.json()})
-                .then((result) => {
-                    debugger
-                    console.log('result', result)
-                    // setFileList(result);
-                })
+            getData(`/?open=${item.filePath}`, true)
+            //     fetch(`http://localhost:4000/?open=${item.filePath}`)
+            //         .then(res => {
+            //             return res.text()
+            //         })
+            //         .then((result) => {
+            //             console.log('result', result)
+            //             setFileData(result);
+            //         })
         }
-
-    },[])
+    }, [])
 
     return (
         <div className="App">
             {
-                fileList.length ? <MyTable arr = {fileList} cb = {clickHandler}/> : <div>Loading...</div>
-
+                fileList.length ? <MyTable arr={fileList} cb={clickHandler} fileData={fileData}/> :
+                    <div>Loading...</div>
             }
-            {/*{*/}
-            {/*    fileList.map((data) => {*/}
-            {/*        return <div key={data.fileName}>*/}
-            {/*            <span>{data.fileName}</span> <span>Size: {data.size}</span>*/}
-
-            {/*        </div>*/}
-
-            {/*    })*/}
-            {/*}*/}
         </div>
     );
 }
